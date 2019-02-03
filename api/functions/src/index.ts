9d9e94a2de8d4203cb719b0f9354aa6b7ba260db
tortoise <sdgillen@crimson.ua.edu>
2019-02-02 19:42:37 -0600

    Fetch JSON

--- api/functions/src/index.ts
+++ api/functions/src/index.ts
@@ -1,8 +1,51 @@
 import * as functions from 'firebase-functions';
+import * as req from 'request';
+import * as fs from 'fs';
+import * as path from 'path';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+let BEARER_TOKEN: string = "BEARER_TOKEN";
+
+export const acquireCropBounds = functions.https.onRequest((request, response) => {
+    let bearerToken: string = fs.readFileSync(path.join(__dirname, "../src/" + BEARER_TOKEN)).toString().trim();
+
+    let visionRequest: string = JSON.stringify({
+        "requests":[
+            {
+                "image":{
+                    "source":{
+                        "imageUri": "gs://instarip-1c336.appspot.com/6.png"
+                    }
+                },
+                "features":[
+                    {
+                        "type":"DOCUMENT_TEXT_DETECTION"
+                    }
+                ]
+            }
+        ]
+    });
+
+    req({
+        method: 'POST',
+        port: 443,
+        auth: {
+            'bearer': bearerToken
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        url: "https://vision.googleapis.com/v1/images:annotate",
+        body: visionRequest
+    }, function(error, res, body) {
+        if(error) {
+            response.send(error);
+        }
+        else {
+            response.send(cropBoundsFromVision(body.toString()));
+        }
+    });
 });
+
+function cropBoundsFromVision(body: string): string {
+    return body;
+} 
